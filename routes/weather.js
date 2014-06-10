6e32d4605c4a353d49868b7e0c7a8e3de0d3b114
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-06-10 23:04:31 +0200

    Implementada la consulta del tiempo actual en función de la ciudad solicitada

--- routes/weather.js
+++ routes/weather.js
@@ -1,6 +1,21 @@
+var http = require('http');
 
-var weather = function(template, res){
-  //api.openweathermap.org/data/2.5/weather?id=3117735&lang=sp
+var url = 'http://api.openweathermap.org/data/2.5/weather?lang=sp&q=';
+
+var weather = function(response, city){
+  var queryWeather = url+city;
+  http.get(queryWeather, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+    res.on('end', function() {
+        var weatherResponse = JSON.parse(body)
+        response.end(JSON.stringify(weatherResponse));
+    });
+  }).on('error', function(e) {
+    console.log("Got error: ", e);
+  });
 }
 
-module.exports = weather;
+module.exports.current = weather;
