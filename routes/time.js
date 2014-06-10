9d7267eac34c8699caf4e580abbb0dc8b1cab05a
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-06-10 22:59:31 +0200

    Refactor de cabeceras

--- routes/time.js
+++ routes/time.js
@@ -1,6 +1,6 @@
 var ntpClient = require('ntp-client');
 var moment = require('moment-timezone');
-var header = require('./headers');
+
 
 var time = function(res){
   ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
@@ -8,7 +8,6 @@ var time = function(res){
       console.error(err);
       return;
     }
-    header.setHeaders(res);
     time = moment(date).tz('Europe/Madrid');
     res.end(JSON.stringify({time: time.format()}));
   });
