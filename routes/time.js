28464f690360c5139cf51f466a300404ccb24961
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-08-05 21:00:18 +0200

    Añadido debug y enlace a los literales de internacionalización

--- routes/time.js
+++ routes/time.js
@@ -1,15 +1,23 @@
 var ntpClient = require('ntp-client');
 var moment = require('moment-timezone');
+var debug = require('debug')('wierzba');
 
 
-var time = function(res){
-  ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
+var time = function(res, lang){
+  ntpClient.getNetworkTime('pool.ntp.org', 123, function(err, date) {
+    var locale = require('./i18n/'+lang+'.json');
     if(err) {
       console.error(err);
-      return;
+      date = new Date();
     }
-    time = moment(date).tz('Europe/Madrid');
-    res.end(JSON.stringify({time: time.format()}));
+    time = moment(date).tz("Europe/Madrid");
+    res.end(JSON.stringify({
+      "time": time.format(),
+      "dayOfTheWeek": locale.days[date.getDay()],
+      "month": locale.months[date.getMonth()],
+      "date": time.format('dddd, D MMMM YYYY'),
+      'hours': ''
+    }));
   });
 }
 
