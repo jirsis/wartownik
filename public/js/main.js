248b2c9740a9c23c3adc35ed5bfccbdb31acc78d
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-08-06 00:20:16 +0200

    Movida la lógica de generación de datos al lado servidor

--- public/js/main.js
+++ public/js/main.js
@@ -31,64 +31,25 @@ var updateTime = function(){
   }, 1000);
 }
 
-function roundVal(temp)
-{
-	return Math.round(temp-273.15);
-}
+var updateCurrentWeather = function(){
+	$.getJSON('/current-weather/Madrid,es', function(json, textStatus) {
+    var temp = json.temperature;
+		var wind = json.windSpeed;
+		var iconClass = json.iconName;
+    var sunrise = json.sunrise;
+    var sunset = json.sunset;
 
-function kmh2beaufort(kmh)
-{
-	var speeds = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
-	for (var beaufort in speeds) {
-		var speed = speeds[beaufort];
-		if (speed > kmh) {
-			return beaufort;
-		}
-	}
-	return 12;
-}
-
-
-var updateWeather = function(){
-	var iconTable = {
-		'01d':'wi-day-sunny',
-		'02d':'wi-day-cloudy',
-		'03d':'wi-cloudy',
-		'04d':'wi-cloudy-windy',
-		'09d':'wi-showers',
-		'10d':'wi-rain',
-		'11d':'wi-thunderstorm',
-		'13d':'wi-snow',
-		'50d':'wi-fog',
-		'01n':'wi-night-clear',
-		'02n':'wi-night-cloudy',
-		'03n':'wi-night-cloudy',
-		'04n':'wi-night-cloudy',
-		'09n':'wi-night-showers',
-		'10n':'wi-night-rain',
-		'11n':'wi-night-thunderstorm',
-		'13n':'wi-night-snow',
-		'50n':'wi-night-alt-cloudy-windy'
-	}
-
-
-	$.getJSON('/current-weather/Opole,pl', function(json, textStatus) {
-    var temp = roundVal(json.main.temp);
-		var wind = roundVal(json.wind.speed);
-		var iconClass = iconTable[json.weather[0].icon];
 		var icon = $('<span/>').addClass('icon').addClass('dimmed').addClass('wi').addClass(iconClass);
-    $('.temp').html(icon.clone().wrap('<p>').parent().html()+temp+'&deg;', 1000);
+    $('.temp').html(icon.clone().wrap('<p>').parent().html()+temp+'&deg;');
 
-		var sunrise = new Date(json.sys.sunrise*1000).toTimeString().substring(0,5);
-		var sunset = new Date(json.sys.sunset*1000).toTimeString().substring(0,5);
-		var windString = '<span class="wi wi-strong-wind xdimmed"></span> ' + kmh2beaufort(wind) ;
+		var windString = '<span class="wi wi-strong-wind xdimmed"></span> ' + wind ;
 		var sunString = '<span class="wi wi-sunrise xdimmed"></span> ' + sunrise;
-		if (json.sys.sunrise*1000 < now && json.sys.sunset*1000 > now) {
+		if (json.isDayLight) {
 			sunString = '<span class="wi wi-sunset xdimmed"></span> ' + sunset;
 		}
 		$('.windsun').html(windString+' '+sunString, 1000);
 
-    $('.city').html(json.name);
+    $('.city').html(json.city);
 
 
     // var temp_min = roundVal(json.main.temp_min);
@@ -104,8 +65,8 @@ var updateWeather = function(){
 }
 
 var main = function(){
-  // updateTime();
-  updateWeather();
+  updateTime();
+  updateCurrentWeather();
 }
 
 $(document).ready(main);
--- routes/weather.js
+++ routes/weather.js
@@ -1,9 +1,44 @@
 var http = require('http');
 var debug = require('debug')('wierzba');
+var moment = require('moment-timezone');
 
 var openweathermapUrl = 'http://api.openweathermap.org/data/2.5/';
 var query = '/?lang=sp&q=';
 var nextFiveDaysCommand = 'forecast';
+
+var iconTable = {
+  '01d':'wi-day-sunny',
+  '02d':'wi-day-cloudy',
+  '03d':'wi-cloudy',
+  '04d':'wi-cloudy-windy',
+  '09d':'wi-showers',
+  '10d':'wi-rain',
+  '11d':'wi-thunderstorm',
+  '13d':'wi-snow',
+  '50d':'wi-fog',
+  '01n':'wi-night-clear',
+  '02n':'wi-night-cloudy',
+  '03n':'wi-night-cloudy',
+  '04n':'wi-night-cloudy',
+  '09n':'wi-night-showers',
+  '10n':'wi-night-rain',
+  '11n':'wi-night-thunderstorm',
+  '13n':'wi-night-snow',
+  '50n':'wi-night-alt-cloudy-windy'
+}
+
+var kmh2beaufort = function(kmh)
+{
+  var speeds = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
+  for (var beaufort in speeds) {
+    var speed = speeds[beaufort];
+    if (speed > kmh) {
+      return beaufort;
+    }
+  }
+  return 12;
+}
+
 
 var getOpenWeather = function(response, query){
   debug(query);
@@ -13,8 +48,19 @@ var getOpenWeather = function(response, query){
         body += chunk;
     });
     res.on('end', function() {
-        var weatherResponse = JSON.parse(body)
-        response.end(JSON.stringify(weatherResponse));
+        var weather = JSON.parse(body);
+        var sunrise = moment.unix(weather.sys.sunrise).tz("Europe/Madrid");
+        var sunset = moment.unix(weather.sys.sunset).tz("Europe/Madrid");
+        response.end(JSON.stringify({
+          "temperature": Math.round((weather.main.temp-273.15)*10)/10,
+          "windSpeed": kmh2beaufort(weather.wind.speed),
+          "icon": weather.weather[0].icon,
+          "iconName": iconTable[weather.weather[0].icon],
+          "sunrise": sunrise.format('HH:mm'),
+          "sunset": sunset.format('HH:mm'),
+          "isDayLight": moment().isBefore(sunrise) && moment().isAfter(sunset),
+          "city": weather.name
+        }));
     });
   }).on('error', function(e) {
     console.log("Got error: ", e);
