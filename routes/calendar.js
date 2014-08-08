7c058dd938860ea3ddcdd2ab36133defb0957fdd
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-08-08 20:18:13 +0200

    Completada el api de calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,13 +1,45 @@
 var ical = require('ical');
+var moment = require('moment-timezone');
 var debug = require('debug')('wierzba');
+var config = require('./wierzba');
+var dateFormatter = "DD/MM/YYYY HH:mm:ss";
+
 
 var calendar = function(res){
-  var icalURL = 'https://www.google.com/calendar/ical/tggh6736d5ve3sqmhk46msej6s%40group.calendar.google.com/private-0debcd9675bb0a9ccc8b708ba9bab0e4/basic.ics';
-  debug(icalURL);
+  var icalURL = config.calendar;
+  moment.lang(config.language);
+
+  var now = moment();
+  var nextHour = moment().add('hour', 1);
+
   ical.fromURL(icalURL, {}, function(err, data) {
-    debug(JSON.stringify(data));
+    var vevents = [];
+    for (var k in data){
+      var vevent = {};
+      var ev = data[k]
+      if( ev.type === 'VEVENT'){
+        vevent.start = moment(ev.start).tz(config.timezone)
+        if (nextHour.isBefore(vevent.start)){
+          vevent.start = vevent.start.format(dateFormatter);
+          vevent.end = moment(ev.end).tz(config.timezone);
+          if ( now.isBefore(vevent.end)){
+            vevent.end= vevent.end.format(dateFormatter);
+            vevent.summary = ev.summary;
+            vevents.push(vevent);
+          }
+        }
+      }
+    }
+    vevents.sort(function(a, b){
+      var msA = moment(a.start, dateFormatter).format('X');
+      var msB = moment(b.start, dateFormatter).format('X');
+      return msA - msB;
+    });
+    res.end(JSON.stringify({
+      "calendar": vevents
+      }));
   });
-  res.end(JSON.stringify({status: "ok"}));
+
 }
 
 module.exports = calendar;
